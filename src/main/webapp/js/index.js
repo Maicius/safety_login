a41ece9a8167fa93ff38c42ba8bfda6bb443fa76
mai ciu s <maicius@outlook.com>
2017-04-25 18:18:01 +0800

    解决tomcat启动后不能加载CSS／js文件的问题： 1在spring配置文件中添加静态资源文件目录 2.将js\css文件夹移至webapp根目录下

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -12,8 +12,8 @@ function join_1(){
         document.querySelectorAll('.cont_letras > p')[0].style.left = '320px';
         document.querySelectorAll('.cont_letras > p')[1].style.left = '-320px';
         document.querySelectorAll('.cont_letras > p')[2].style.left = '200px';
-    setTimeout(function(){
-        document.querySelector('.cont_join').className = 'cont_join cont_join_form_act';
+        setTimeout(function(){
+            document.querySelector('.cont_join').className = 'cont_join cont_join_form_act';
         },1000);
     t++;
     }
@@ -23,6 +23,7 @@ function join_1(){
         setTimeout(function(){
 	        document.querySelector('.cont_join').className = 'cont_join cont_join_form_act cont_join_finish';
         },500);
+
   }
 
 }
